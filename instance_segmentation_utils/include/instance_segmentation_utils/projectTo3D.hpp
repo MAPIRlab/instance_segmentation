c86f600b5f28041b859750d90f47f1be6424b9da
Pepe Ojeda <ojedamorala@hotmail.com>
2024-06-14 12:23:37 +0200

    Fixed up projectTo3D

--- instance_segmentation_utils/include/instance_segmentation_utils/projectTo3D.hpp
+++ instance_segmentation_utils/include/instance_segmentation_utils/projectTo3D.hpp
@@ -1,20 +1,35 @@
+#pragma once
+#include <image_geometry/pinhole_camera_model.h>
 #include <instance_segmentation_utils/BufferWrapper.h>
+#include <message_filters/subscriber.h>
+#include <message_filters/sync_policies/approximate_time.h>
+#include <message_filters/sync_policies/exact_time.h>
+#include <message_filters/synchronizer.h>
 #include <opencv2/core.hpp>
 #include <rclcpp/rclcpp.hpp>
-#include <vision_msgs/msg/detection3_d_array.hpp>
 #include <segmentation_msgs/srv/segment_image.hpp>
+#include <sensor_msgs/image_encodings.hpp>
+#include <sensor_msgs/msg/camera_info.hpp>
 #include <sensor_msgs/msg/compressed_image.hpp>
+#include <vision_msgs/msg/detection3_d_array.hpp>
+#include <visualization_msgs/msg/marker.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
 
-using Image = sensor_msgs::msg::CompressedImage;
+using sensor_msgs::msg::PointCloud2;
+using sensor_msgs::msg::CameraInfo;
+using Image = sensor_msgs::msg::Image;
 using Detection3D = vision_msgs::msg::Detection3D;
 using Detection3DArray = vision_msgs::msg::Detection3DArray;
+using SyncPolicy = message_filters::sync_policies::ApproximateTime<Image, Image, CameraInfo>;
+using visualization_msgs::msg::Marker;
 
-struct CameraCalibration
+struct MinMaxBounds
 {
-    float centerX, centerY;
-    float focalX, focalY;
-    float depthLimit;
+    geometry_msgs::msg::Point min;
+    geometry_msgs::msg::Point max;
 };
+
+enum class DepthType{Unity, Millimiters};
 
 class ProjectTo3D : public rclcpp::Node
 {
@@ -25,13 +40,37 @@ public:
 
 private:
     void projectInstancesAndPublish(segmentation_msgs::srv::SegmentImage::Response::SharedPtr response, const cv::Mat& depth,
-                                    std_msgs::msg::Header image_header);
+                                    const image_geometry::PinholeCameraModel& cameraModel, std_msgs::msg::Header image_header);
 
-    CameraCalibration m_cameraCalibration;
+    void imageCallback(const Image::ConstSharedPtr& colorMsg, const Image::ConstSharedPtr& depthMsg, const CameraInfo::ConstSharedPtr& cameraInfo);
+    float depthToMeters(double depth, DepthType type);
+    MinMaxBounds getBoundsSingleMask(const cv::Mat& mask, const cv::Mat& coord3D, const std_msgs::msg::Header& header);
+    void clearMarkers();
+    void visualizeBB(const vision_msgs::msg::BoundingBox3D& box);
+    void visualizePointCloud(const cv::Mat& mask, const cv::Mat& coord3D, const std_msgs::msg::Header& header);
 
-    Image::SharedPtr m_lastImage;
-    rclcpp::Subscription<Image>::SharedPtr m_cameraSub;
+    struct Message
+    {
+        Image::ConstSharedPtr colorImage;
+        Image::ConstSharedPtr depthImage;
+        CameraInfo::ConstSharedPtr cameraInfo;
+        bool valid()
+        {
+            return colorImage != nullptr;
+        }
+    };
+    Message m_lastMessage;
+    message_filters::Subscriber<Image> m_colorSub;
+    message_filters::Subscriber<Image> m_depthSub;
+    message_filters::Subscriber<CameraInfo> m_cameraInfoSub;
+    std::shared_ptr<message_filters::Synchronizer<SyncPolicy>> synchronizer; // TODO does this need to be a shared_ptr?
+
     rclcpp::Client<segmentation_msgs::srv::SegmentImage>::SharedPtr m_detectronClient;
     rclcpp::Publisher<Detection3DArray>::SharedPtr m_pub3D;
+    rclcpp::Publisher<Marker>::SharedPtr m_markerPub;
+    rclcpp::Publisher<PointCloud2>::SharedPtr m_pclPub;
     BufferWrapper m_tfBuffer;
+
+    std::string m_colorFormat;
+    std::string m_depthFormat;
 };
--- instance_segmentation_utils/src/projectTo3D.cpp
+++ instance_segmentation_utils/src/projectTo3D.cpp
@@ -1,5 +1,8 @@
 #include <cv_bridge/cv_bridge.h>
+#include <fstream>
 #include <instance_segmentation_utils/projectTo3D.hpp>
+#include <opencv2/highgui.hpp>
+#include <pcl_conversions/pcl_conversions.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
 
 int main(int argc, char** argv)
@@ -24,76 +27,109 @@ ProjectTo3D::ProjectTo3D() : Node("ProjectInstancesTo3D"), m_tfBuffer(get_clock(
 {
     using namespace std::placeholders;
 
-    std::string camera_topic = declare_parameter<std::string>("camera_topic", "camera");
-    m_cameraSub = create_subscription<Image>(camera_topic, 5, [this](const Image::SharedPtr msg) { m_lastImage = msg; });
+    std::string color_topic = declare_parameter<std::string>("color_topic", "camera/color/raw");
+    std::string depth_topic = declare_parameter<std::string>("depth_topic", "camera/depth/raw");
+    std::string camerainfo_topic = declare_parameter<std::string>("info_topic", "camera/info");
+    m_colorSub.subscribe(this, color_topic);
+    m_depthSub.subscribe(this, depth_topic);
+    m_cameraInfoSub.subscribe(this, camerainfo_topic);
+    synchronizer = std::make_shared<message_filters::Synchronizer<SyncPolicy>>(SyncPolicy(5), m_colorSub, m_depthSub, m_cameraInfoSub);
+    synchronizer->registerCallback(std::bind(&ProjectTo3D::imageCallback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3));
+    RCLCPP_INFO(get_logger(),
+                "Using topics:\n"
+                "   color: %s\n"
+                "   depth: %s\n"
+                "   info: %s",
+                color_topic.c_str(), depth_topic.c_str(), camerainfo_topic.c_str());
 
-    m_detectronClient = create_client<segmentation_msgs::srv::SegmentImage>("/detectron/segment");
+    m_colorFormat = declare_parameter<std::string>("color_format", "rgb8");
+    m_depthFormat = declare_parameter<std::string>("depth_format", "rgb8");
+
+    const char* detectronTopic = "/detectron/segment";
+    m_detectronClient = create_client<segmentation_msgs::srv::SegmentImage>(detectronTopic);
     m_pub3D = create_publisher<Detection3DArray>("semantic_instances_3D", 10);
+    m_markerPub = create_publisher<Marker>("/semanticObject_visualization", 10);
+    m_pclPub = create_publisher<PointCloud2>("/semanticPointCloud", 10);
 
-    m_cameraCalibration.centerX = declare_parameter<float>("imageCenter_x", 320);
-    m_cameraCalibration.centerY = declare_parameter<float>("imageCenter_y", 240);
-
-    m_cameraCalibration.focalY = declare_parameter<float>("focalDistance_y", 457.14);
-    m_cameraCalibration.focalY = declare_parameter<float>("focalDistance_y", 470.58);
-
-    m_cameraCalibration.depthLimit = declare_parameter<float>("depthLimit", 15.0);
-
-    while (!m_detectronClient->wait_for_service(std::chrono::seconds(5)))
-        RCLCPP_INFO(get_logger(), "Waiting for service to become available...");
+    RCLCPP_INFO(get_logger(), "Expecting detectron service in: %s", detectronTopic);
+    while (!m_detectronClient->wait_for_service(std::chrono::seconds(5))) RCLCPP_INFO(get_logger(), "Waiting for service to become available...");
+    RCLCPP_INFO(get_logger(), "Found detectron, ready for action!");
 }
 
 void ProjectTo3D::processImage()
 {
-    if (!m_lastImage)
+    if (!m_lastMessage.valid())
         return;
 
-    cv::Mat rgb, depth;
-    // split the image into rgb and d
-    {
-        cv::Mat channels[4];
-        cv_bridge::CvImagePtr receivedImageCV = cv_bridge::toCvCopy(*m_lastImage);
-        cv::split(receivedImageCV->image, channels);
+    RCLCPP_INFO(get_logger(), "Processing one image pair");
 
-        cv::merge(channels, 3, rgb);
-        depth = channels[3];
+    // copy the info, decompressing images if necessary
+    std_msgs::msg::Header header = m_lastMessage.colorImage->header;
+    cv::Mat rgb = cv_bridge::toCvCopy(m_lastMessage.colorImage, m_colorFormat)->image;
+
+    cv::Mat depth;
+    // For some reason cv_bridge does not correctly parse mono16 images. Instead of a single 16 bit channel, it creates a cv::Mat with 2 8 bit
+    // channels
+    if (m_depthFormat == "mono16")
+    {
+        uint8_t* depthBytes = (uint8_t*)m_lastMessage.depthImage->data.data();
+        depth = cv::Mat((int)m_lastMessage.depthImage->height, (int)m_lastMessage.depthImage->width, CV_16U, depthBytes).clone();
     }
+    else
+        depth = cv_bridge::toCvCopy(m_lastMessage.depthImage, m_depthFormat)->image;
+
+    image_geometry::PinholeCameraModel cameraModel;
+    cameraModel.fromCameraInfo(*m_lastMessage.cameraInfo);
+
+    // clean up the buffer before spinning
+    m_lastMessage.colorImage = nullptr;
+    m_lastMessage.depthImage = nullptr;
+    m_lastMessage.cameraInfo = nullptr;
 
     // send the rgb image to detectron
     auto request = std::make_shared<segmentation_msgs::srv::SegmentImage::Request>();
-    cv_bridge::CvImage(m_lastImage->header, "rgb8", rgb).toImageMsg(request->image);
-    std_msgs::msg::Header header = m_lastImage->header;
-    m_lastImage = nullptr;
+    cv_bridge::CvImage(header, m_colorFormat, rgb).toImageMsg(request->image);
 
     auto future = m_detectronClient->async_send_request(request);
-    if (rclcpp::spin_until_future_complete(shared_from_this(), future) == rclcpp::FutureReturnCode::SUCCESS)
-        projectInstancesAndPublish(future.get(), depth, header);
-
-    else
-        RCLCPP_ERROR(get_logger(), "Could not access the detectron service!");
+    try
+    {
+        if (rclcpp::spin_until_future_complete(shared_from_this(), future) == rclcpp::FutureReturnCode::SUCCESS)
+            projectInstancesAndPublish(future.get(), depth, cameraModel, header);
+        else
+            RCLCPP_ERROR(get_logger(), "Could not access the detectron service!");
+    }
+    catch (const std::exception& e)
+    {
+        RCLCPP_ERROR(get_logger(), "Exception while processing image: %s", e.what());
+    }
 }
 
 void ProjectTo3D::projectInstancesAndPublish(segmentation_msgs::srv::SegmentImage::Response::SharedPtr response, const cv::Mat& depth,
-                                             std_msgs::msg::Header image_header)
+                                             const image_geometry::PinholeCameraModel& cameraModel, std_msgs::msg::Header image_header)
 {
-    cv::Mat x_coord(cv::Size(depth.rows, depth.cols), CV_32FC1);
-    cv::Mat y_coord(cv::Size(depth.rows, depth.cols), CV_32FC1);
-    cv::Mat z_coord(cv::Size(depth.rows, depth.cols), CV_32FC1);
+    clearMarkers();
+
+    // 3D coordinates of the point that corresponds to each specific pixel in the image (camera space)
+    cv::Mat coord3D(depth.rows, depth.cols, CV_32FC3);
     {
-        #pragma omp parallel for collapse(2)
-        for (int column = 0; column < x_coord.cols; column++)
+#pragma omp parallel for collapse(2)
+        for (int column = 0; column < coord3D.cols; column++)
         {
-            for (int row = 0; row < x_coord.rows; row++)
+            for (int row = 0; row < coord3D.rows; row++)
             {
-                float z = depth.at<uint8_t>(row, column) / 255.0 * m_cameraCalibration.depthLimit;
-                x_coord.at<float>(column, row) = (m_cameraCalibration.centerX - column) * (z / m_cameraCalibration.focalX);
-                y_coord.at<float>(column, row) = (m_cameraCalibration.centerY - row) * (z / m_cameraCalibration.focalY);
-                z_coord.at<float>(column, row) = z;
+                // TODO make the type be inferred from m_depthFormat
+                float z = depthToMeters(depth.at<uint16_t>(row, column), DepthType::Unity);
+
+                cv::Point2d imagePoint = cameraModel.rectifyPoint(cv::Point2d(column, row));
+                cv::Point3d rayDirection = cameraModel.projectPixelTo3dRay(imagePoint);
+                cv::Point3d point3D = rayDirection * z;
+
+                coord3D.at<cv::Vec3f>(row, column) = cv::Vec3f(point3D.x, point3D.y, point3D.z);
             }
         }
     }
 
     // process the instances
-
     Detection3DArray objectsArray;
     for (const segmentation_msgs::msg::SemanticInstance2D& instance : response->instances)
     {
@@ -102,47 +138,144 @@ void ProjectTo3D::projectInstancesAndPublish(segmentation_msgs::srv::SegmentImag
 
         detection.results = instance.detection.results;
 
-        cv_bridge::CvImagePtr mask = cv_bridge::toCvCopy(instance.mask);
+        cv::Mat mask = cv_bridge::toCvCopy(instance.mask)->image;
+        // cv::imshow("mask", mask->image);
+        // cv::waitKey(30);
 
-        // bandpass filter depth data. Lifted straight from the other implementation, don't ask me why this is a thing
-        {
-            double min_z, max_z;
-            cv::minMaxIdx(z_coord, &min_z, &max_z, nullptr, nullptr, mask->image);
-            float top_margin = (max_z - min_z) * 0.9 + min_z;
-            float bottom_margin = (max_z - min_z) * 0.1 + min_z;
+        cv::erode(mask, mask, cv::Mat::ones(10,10, CV_8U));
 
-            for (int column = 0; column < mask->image.cols; column++)
-            {
-                for (int row = 0; row < mask->image.rows; row++)
-                {
-                    float z = z_coord.at<float>(column, row);
-                    if (z < bottom_margin || z > top_margin)
-                        mask->image.at<float>(column, row) = 0;
-                }
-            }
-        }
-
-        double x_min, x_max, y_min, y_max, z_min, z_max;
-        cv::minMaxIdx(x_coord, &x_min, &x_max, nullptr, nullptr, mask->image);
-        cv::minMaxIdx(y_coord, &y_min, &y_max, nullptr, nullptr, mask->image);
-        cv::minMaxIdx(z_coord, &z_min, &z_max, nullptr, nullptr, mask->image);
-
-        geometry_msgs::msg::TransformStamped cameraToMap =
-            m_tfBuffer.buffer.lookupTransform(image_header.frame_id, "map", tf2_ros::fromMsg(image_header.stamp));
+        // Get the masked point cloud, and record the min and max coords in world
+        MinMaxBounds bounds = getBoundsSingleMask(mask, coord3D, image_header);
+        visualizePointCloud(mask, coord3D, image_header);
 
         detection.header.frame_id = "map";
         detection.header.stamp = image_header.stamp;
-        
-        detection.bbox.size.x = x_max - x_min;
-        detection.bbox.size.y = y_max - y_min;
-        detection.bbox.size.z = z_max - z_min; // originally, this was the stdDev of the z values in the mask
-        tf2::doTransform(detection.bbox.size, detection.bbox.size, cameraToMap);
 
-        detection.bbox.center.position.x = x_min + detection.bbox.size.x * 0.5;
-        detection.bbox.center.position.y = y_min + detection.bbox.size.y * 0.5;
-        detection.bbox.center.position.z = z_min + detection.bbox.size.z * 0.5;
-        tf2::doTransform(detection.bbox.center, detection.bbox.center, cameraToMap);
+        detection.bbox.size.x = bounds.max.x - bounds.min.x;
+        detection.bbox.size.y = bounds.max.y - bounds.min.y;
+        detection.bbox.size.z = bounds.max.z - bounds.min.z;
+
+        detection.bbox.center.position.x = bounds.min.x + detection.bbox.size.x * 0.5;
+        detection.bbox.center.position.y = bounds.min.y + detection.bbox.size.y * 0.5;
+        detection.bbox.center.position.z = bounds.min.z + detection.bbox.size.z * 0.5;
+        visualizeBB(detection.bbox);
     }
 
     m_pub3D->publish(objectsArray);
+}
+
+MinMaxBounds ProjectTo3D::getBoundsSingleMask(const cv::Mat& mask, const cv::Mat& coord3D, const std_msgs::msg::Header& header)
+{
+    geometry_msgs::msg::TransformStamped cameraToMap = m_tfBuffer.buffer.lookupTransform("map", header.frame_id, tf2_ros::fromMsg(header.stamp));
+
+    geometry_msgs::msg::Point min;
+    min.x = DBL_MAX;
+    min.y = DBL_MAX;
+    min.z = DBL_MAX;
+    geometry_msgs::msg::Point max;
+    max.x = -DBL_MAX;
+    max.y = -DBL_MAX;
+    max.z = -DBL_MAX;
+    for (int i = 0; i < mask.rows; i++)
+    {
+        for (int j = 0; j < mask.cols; j++)
+        {
+            if (mask.at<uint8_t>(i, j) != 0)
+            {
+                geometry_msgs::msg::Point cameraPoint;
+                cameraPoint.x = coord3D.at<cv::Vec3f>(i,j)[0];
+                cameraPoint.y = coord3D.at<cv::Vec3f>(i,j)[1];
+                cameraPoint.z = coord3D.at<cv::Vec3f>(i,j)[2];
+
+                // transform point cloud to world space to get the AABB
+                geometry_msgs::msg::Point worldPoint;
+                tf2::doTransform(cameraPoint, worldPoint, cameraToMap);
+                min.x = std::min(min.x, worldPoint.x);
+                min.y = std::min(min.y, worldPoint.y);
+                min.z = std::min(min.z, worldPoint.z);
+
+                max.x = std::max(max.x, worldPoint.x);
+                max.y = std::max(max.y, worldPoint.y);
+                max.z = std::max(max.z, worldPoint.z);
+            }
+        }
+    }
+    return {min, max};
+}
+
+void ProjectTo3D::imageCallback(const Image::ConstSharedPtr& colorMsg, const Image::ConstSharedPtr& depthMsg,
+                                const CameraInfo::ConstSharedPtr& cameraInfo)
+{
+    m_lastMessage.colorImage = colorMsg;
+    m_lastMessage.depthImage = depthMsg;
+    m_lastMessage.cameraInfo = cameraInfo;
+}
+
+float ProjectTo3D::depthToMeters(double depth, DepthType type)
+{
+    if (type == DepthType::Unity)
+    {
+        constexpr float max_range = 10.0f;
+        return (depth / std::numeric_limits<uint16_t>::max()) * max_range;
+    }
+    else if (type == DepthType::Millimiters)
+    {
+        return depth * 0.001;
+    }
+    else
+    {
+        RCLCPP_ERROR(get_logger(), "DepthType %d not supported", (int)type);
+        throw std::exception();
+    }
+}
+
+void ProjectTo3D::clearMarkers()
+{
+    Marker marker;
+    marker.header.frame_id = "map";
+    marker.header.stamp = now();
+    marker.action = Marker::DELETEALL;
+    m_markerPub->publish(marker);
+}
+
+void ProjectTo3D::visualizeBB(const vision_msgs::msg::BoundingBox3D& box)
+{
+    Marker marker;
+    marker.header.frame_id = "map";
+    marker.header.stamp = now();
+    marker.action = Marker::ADD;
+    marker.type = Marker::CUBE;
+    marker.pose = box.center;
+    marker.scale = box.size;
+
+    marker.color.r = 1;
+    marker.color.g = 1;
+    marker.color.b = 1;
+    marker.color.a = 1;
+
+    m_markerPub->publish(marker);
+}
+
+void ProjectTo3D::visualizePointCloud(const cv::Mat& mask, const cv::Mat& coord3D, const std_msgs::msg::Header& header)
+{
+    pcl::PointCloud<pcl::PointXYZ> pcl;
+    for (int i = 0; i < mask.rows; i++)
+    {
+        for (int j = 0; j < mask.cols; j++)
+        {
+            if (mask.at<uint8_t>(i, j) != 0)
+            {
+                geometry_msgs::msg::Point cameraPoint;
+                cameraPoint.x = coord3D.at<cv::Vec3f>(i,j)[0];
+                cameraPoint.y = coord3D.at<cv::Vec3f>(i,j)[1];
+                cameraPoint.z = coord3D.at<cv::Vec3f>(i,j)[2];
+                pcl::PointXYZ pcl_point(cameraPoint.x, cameraPoint.y, cameraPoint.z);
+                pcl.points.push_back(pcl_point);
+            }
+        }
+    }
+    PointCloud2 pointMsg;
+    pcl::toROSMsg(pcl, pointMsg);
+    pointMsg.header = header;
+    m_pclPub->publish(pointMsg);
 }
